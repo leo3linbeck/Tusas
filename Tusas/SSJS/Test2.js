1a721121e592c980a2f1ed67acee314b207051a1
leo3linbeck <leo3@linbeck.com>
2013-11-14 13:13:29 -0600

    Test2 and index.html

--- Tusas/SSJS/Test2.js
+++ Tusas/SSJS/Test2.js
@@ -1,1 +1,1 @@
-﻿ds.Family.all().remove();ds.Person.all().remove();include('SSJS/GenerateMockData.js');L3.generateMockData();ds.Person.all().toArray('fullName, gender, relationship, motherOf.ID, fatherOf.ID');
+﻿//ds.Family.all().remove();//ds.Person.all().remove();include('SSJS/GenerateMockData.js');L3.generateMockData();ds.Person.all().toArray('fullName, gender, relationship, motherOf.ID, fatherOf.ID');
